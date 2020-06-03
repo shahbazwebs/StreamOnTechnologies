036d91c9733f8e85114bf1f9867109cd57965797
ShahbazAkhtar <shahbaz.hydb@gmail.com>
2020-06-04 00:17:29 +0530

    upload folder

---
+++ src/app/components/shared/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  isSticky: boolean = false;
+
+  constructor() { }
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= 250;
+  }
+  ngOnInit() {
+
+    
+  }
+
+}
